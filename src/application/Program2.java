0f08ffade271947531e520b3294fc98f5332cf2c
Miguel Teles <miguelteles2002@hotmail.com>
2022-08-27 15:46:52 -0300

    Finally block to close PreparedStatements and Insert method implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -10,11 +12,18 @@ public class Program2 {
 		
 		DepartmentDao depDao = DaoFactory.createDepartmentDao();
 		
-		System.out.println("=== TESTE 1: findById DEPARTMENT");
+		System.out.println("=== TESTE 1: findById DEPARTMENT ===");
 		Department dep = depDao.findById(1);
 		System.out.println(dep);
 		
+		System.out.println("\n === TESTE 2: findAll DEPARTMENT");
+		List<Department> listDep = depDao.findAll();
+		listDep.forEach(System.out::println);
 		
+		System.out.println("\n === TESTE 2: findAll DEPARTMENT");
+		Department newDep = new Department(null,"Money");
+		depDao.insert(newDep);
+		System.out.println("Done! New department created!");
 		
 		
 
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -4,8 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
+import db.DB;
 import db.DBException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -20,7 +23,33 @@ private Connection conn;
 	
 	@Override
 	public void insert(Department obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;	
+		
+		
+		try {
+			st = conn.prepareStatement("INSERT INTO Department "
+					+ "(Name) VALUES (?)", Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, obj.getName());
+			
+			int rowsAffected = st.executeUpdate();
+			if(rowsAffected>0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if(rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+				}
+				
+				DB.closeResultSet(rs);
+				
+			} else {
+				throw new DBException("Unexpected erro! No rows affected");
+			}
+		} catch(SQLException e) {
+			throw new DBException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
@@ -52,13 +81,32 @@ private Connection conn;
 			return null;
 		} catch(SQLException e) {
 			throw new DBException(e.getMessage());
+		} finally{
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
 		}
 	}
 
 	@Override
 	public List<Department> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		List<Department> list = new ArrayList<>();
+		
+		try {
+			st = conn.prepareStatement("SELECT * FROM Department");			
+			rs = st.executeQuery();
+			while(rs.next()) {
+				list.add(new Department(rs.getInt("Id"), rs.getString("Name")));
+			}
+			
+			return list;
+		} catch(SQLException e) {
+			throw new DBException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 }
